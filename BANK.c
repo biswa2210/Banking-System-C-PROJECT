8cae04872327488fca87ce7ff0538ab0662a9aee
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2022-02-26 19:09:01 +0530

    CREATED BY BISWA

--- BANK.c
+++ BANK.c
@@ -2,6 +2,11 @@
 #include<stdlib.h>
 #include<windows.h>
 int i,j;
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 int main_exit;
 void menu();
 struct date{
